28d586ca1d5b5da62dc11e8982994676e9676539
Adrià Pallejà <adria.palleja@gmail.com>
2018-09-19 16:09:39 +0200

    Firebase Web SDK Initialization

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 
 export default class App extends React.Component {
+constructor(props) {
+  super(props);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBXuy-OoNPmJ0IwihwmnkPLp8dcqHaWrh8",
+    authDomain: "fairprice-32f08.firebaseapp.com",
+    databaseURL: "https://fairprice-32f08.firebaseio.com",
+    projectId: "fairprice-32f08",
+    storageBucket: "fairprice-32f08.appspot.com",
+    messagingSenderId: "176853120503"
+  };
+  firebase.initializeApp(config);
+}
+
   render() {
     return (
       <View style={styles.container}>
