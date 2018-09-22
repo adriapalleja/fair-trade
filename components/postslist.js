c26232295a7252371e8a0ec724059de5d25448ad
Adrià Pallejà <adria.palleja@gmail.com>
2018-09-22 12:59:53 +0200

    Unsplash pictures for post and request cards

--- components/postslist.js
+++ components/postslist.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View, AsyncStorage, StyleSheet } from 'react-native';
-import { List, ListItem } from 'react-native-elements'
+import { View, Text, Button, AsyncStorage, StyleSheet } from 'react-native';
+import { List, Card } from 'react-native-elements'
 import * as data from  '../containers/firebase';
+import axios from 'axios';
+
+const APP_ID = 'd893796f899f3899e6b9878cc2cd1e3327b26111967a6c92b62da9df52194989';
 
 export default class PostsList extends React.Component {
   static navigationOptions = {
@@ -20,9 +23,21 @@ export default class PostsList extends React.Component {
     data.postsRef.on('value', (snapshot) => {
       let val = snapshot.val();
       let items = Object.values(val);
-      this.setState({posts:items});
+      this._getImages(items);
     });
     this._getUser();
+  }
+
+  async _getImages(items) {
+    items.forEach((item)=>{
+      axios.get('https://api.unsplash.com/search/photos/?query='+item.product+'&client_id=' + APP_ID)
+		.then(data => {
+      item.img = data.data.results[0].links.download;
+      this.setState({posts:items});
+		}).catch(err => {
+			console.log('Error happened during fetching!', err);
+		});
+    })
   }
 
   async _getUser() {
@@ -36,12 +51,15 @@ export default class PostsList extends React.Component {
 
   render() {
     let posts = this.state.posts.map((item) => {
-      let length = 0;
-      if (item.interested) length = item.interested.length;
-      let subtitle = item.receiver ? 'Finished' : length + ' interested';
-      if (item.poster_id === this.state.user_id) {
-        return <ListItem key={item.id} subtitle={subtitle}
-          title={item.product+' '+item.quantity+' kg'} onPressRightIcon={()=>this.onProductDetails(item)}/>
+    let length = 0;
+    if (item.interested) length = item.interested.length;
+    let subtitle = item.receiver ? 'Finished' : length + ' interested';
+    if (item.poster_id === this.state.user_id) {
+      return <Card image={{uri:item.img}} key={item.id} title={item.product+' '+item.quantity+' kg'}>
+        <Text>{subtitle}</Text>
+        <Button icon={{name: 'code'}} color='#42b97c' title='Post Details'
+          onPress={()=>this.onProductDetails(item)}/>
+            </Card>
       }
     });
 
--- components/requestslist.js
+++ components/requestslist.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { List, ListItem } from 'react-native-elements'
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { List, Card } from 'react-native-elements'
 import * as data from  '../containers/firebase';
+import axios from 'axios';
+
+const APP_ID = 'd893796f899f3899e6b9878cc2cd1e3327b26111967a6c92b62da9df52194989';
 
 export default class RequestsList extends React.Component {
   static navigationOptions = {
@@ -19,8 +22,20 @@ export default class RequestsList extends React.Component {
     data.postsRef.on('value', (snapshot) => {
       let val = snapshot.val();
       let items = Object.values(val);
-      this.setState({posts:items});
+      this._getImages(items);
     });
+  }
+
+  async _getImages(items) {
+    items.forEach((item)=>{
+      axios.get('https://api.unsplash.com/search/photos/?query='+item.product+'&client_id=' + APP_ID)
+		.then(data => {
+      item.img = data.data.results[0].links.download;
+      this.setState({posts:items});
+		}).catch(err => {
+			console.log('Error happened during fetching!', err);
+		});
+    })
   }
 
   onRequestDetails(request) {
@@ -30,8 +45,11 @@ export default class RequestsList extends React.Component {
   render() {
     let requests = this.state.posts.map((item) => {
       if (!item.receiver) {
-        return <ListItem key={item.id} subtitle={item.location}
-        title={item.quantity+' kg '+item.product} onPressRightIcon={()=>this.onRequestDetails(item)}/>
+        return <Card image={{uri:item.img}} key={item.id} title={item.product+' '+item.quantity+' kg'}>
+        <Text>{item.location}</Text>
+        <Button icon={{name: 'code'}} color='#42b97c' title='Post Details'
+          onPress={()=>this.onRequestDetails(item)}/>
+            </Card>
       }
     });
 
